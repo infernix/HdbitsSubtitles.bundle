9d29119137e9951fbdcfee1777357973a551a96b
Coil <notvalid@whatnot.com>
2011-06-24 00:12:59 +0200

    Fixes for better tv show searches and other cleanups

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -14,6 +14,14 @@ langPrefs2HDbits = {'eng':'uk', 'swe':'se', 'ara':'ar', 'bul':'bg', 'chi':'cn', 
 #Find these in the DefaultPrefs.json and http://dev.plexapp.com/docs/api/localekit.html#locale-language
 langPrefs2Plex = {'eng':'en', 'swe':'sv', 'ara':'ar', 'bul':'bg', 'chi':'zh', 'hrv':'hr', 'cze':'cs', 'dan':'da', 'dut':'nl', 'est':'et', 'fin':'fi', 'fre':'fr', 'ger':'de', 'gre':'el', 'heb':'he', 'hun':'hu', 'ita':'it', 'jpn':'ja', 'kor':'ko', 'nor':'no', 'pol':'pl', 'por':'pt', 'ron':'ro', 'rus':'ru', 'srp':'sr', 'slo':'sk', 'slv':'sl', 'spa':'es', 'tur':'tr'}
 
+class SubInfo():
+    def __init__(self, lang, url, sub, name):
+        self.lang = lang
+        self.url = url
+        self.sub = sub
+        self.name = name
+        self.ext = string.split(self.name, '.')[-1]
+
 def Start():
     HTTP.CacheTime = 0
     HTTP.Headers['User-agent'] = OS_PLEX_USERAGENT
@@ -42,29 +50,20 @@ def getLangList():
 
     return langList
 
+def getSnEString(season, episode):
+    return "S" + str(season).zfill(2) + "E" + str(episode).zfill(2)
 
 #Do a basic search for the filename and return all sub urls found
-def simpleSearch(fileName, lang = 'eng'):
+def simpleSearch(searchString, lang = 'eng'):
 #First make sure we are logged in
     checkLogin() 
-
-#Prepare the filename for search by removing the suffix and other nuisances
-    lastDotIndex = string.rfind(fileName, '.')
-    if(lastDotIndex > -1):
-        fileName = fileName[0:lastDotIndex]
-
-    fileName = string.replace(fileName, '.', ' ')
-    fileName = string.replace(fileName, '(',' ')
-    fileName = string.replace(fileName, ')',' ')
-    fileName = string.replace(fileName, '-',' ')
-    Log("Searching for: %s" % fileName)
-
-    urlName = String.Quote(fileName, usePlus=True)
-    Log("escaped file name: %s" % urlName)
+    
+    Log("Searching for: %s" % searchString)
+    urlName = String.Quote(searchString, usePlus=True)
+    Log("escaped: %s" % urlName)
     searchUrl = HDBITS_SEARCH_PAGE % (langPrefs2HDbits[lang], urlName)
     Log("searchUrl: %s" % searchUrl)
     elem = HTML.ElementFromURL(searchUrl)
-    #Log("Search page: %s" % HTML.StringFromElement(elem))
 
 #Find all sub urls on this page and return the link/url to them
     subtitles = elem.xpath("//a[starts-with(@href,'getdox.php')]/@href")
@@ -73,21 +72,23 @@ def simpleSearch(fileName, lang = 'eng'):
 
     return subtitles
 
-class SubInfo():
-    lang = None
-    url = None
-    sub = None
-    subExt = None
+def getReleaseGroup(filename, override = False):
+    if(Prefs["UseReleaseGroup"] == True or override == True):
+        tmpFile = string.replace(filename, '-', '.')
+        splitName = string.split(tmpFile, '.')
+        group = splitName[-2]
+        return group
+    else:
+        return ""
 
-def getSubsForPart(part):
-    fileName = os.path.basename(part.file)
-    Log("Filename: %s" % fileName)
+def getSubsForPart(searchString):
+    Log("SearchString: %s" % searchString)
 #For each language defined in preferences, do a search
     subsList = []
     for lang in getLangList():
         plexLang = Locale.Language.Match(langPrefs2Plex[lang])
         Log("LangTest: %s" % plexLang)
-        subtitleUrls = simpleSearch(fileName, lang)
+        subtitleUrls = simpleSearch(searchString, lang)
 #For each sub found. Fetch it and hand it over to Plex with the right language and suffix set
         for url in subtitleUrls:
             splitUrl = string.split(url, '/')
@@ -100,21 +101,44 @@ def getSubsForPart(part):
             if subExt in subtitleExt:
                 Log("subExt %s is ok" % subExt)
                 sub = HTTP.Request(subUrl, immediate=True).content
-                si = SubInfo()
-                si.lang = plexLang
-                si.url = url
-                si.sub = sub
-                si.subExt = subExt
+                si = SubInfo(plexLang, subUrl, sub, subUrl)
                 subsList.append(si)
+            elif subExt == "zip":
+                zipArchive = Archive.ZipFromURL(subUrl)
+                for name in zipArchive:
+                    Log("Name in zip: %s" % name)
+                    subData = zipArchive[name]
+                    si = SubInfo(lang, subUrl, subData, name)
+                    subsList.append(si)
             else:
                 Log("Can't handle sub of type: %s" % subExt)
     return subsList
 
 class HdbitsSubtitlesAgentMovies(Agent.Movies):
-    name = 'HDBits.org Subtitles Movies'
+    name = 'HDBits.org Movie Subtitles'
     languages = [Locale.Language.English]
     primary_provider = False
     contributes_to = ['com.plexapp.agents.imdb']
+
+    def getMovieSearchString(self, fileName):
+        Log("getMovieSearchString, fileName: %s" % fileName)
+        releaseGroup = getReleaseGroup(fileName, True)
+        #Prepare the filename for search by removing the suffix and other nuisances
+        lastDotIndex = string.rfind(fileName, '.')
+        if(lastDotIndex > -1):
+            searchString = fileName[0:lastDotIndex]
+    
+        Log("ReleaseGroup: %s" % releaseGroup)
+        searchString = string.replace(searchString, releaseGroup, "")
+        
+        searchString = string.replace(searchString, '.', ' ')
+        searchString = string.replace(searchString, '(',' ')
+        searchString = string.replace(searchString, ')',' ')
+        searchString = string.replace(searchString, '-',' ')
+        
+        searchString = searchString + getReleaseGroup(fileName)
+        
+        return searchString.strip() 
 
     def search(self, results, media, lang):
         Log("MOVIE SEARCH CALLED")    
@@ -124,13 +148,10 @@ class HdbitsSubtitlesAgentMovies(Agent.Movies):
             Log("MOVIE UPDATE CALLED")
             for item in media.items:
                 for part in item.parts:
-                    subsList = getSubsForPart(part)
-                    valid_names = list()
+                    subsList = getSubsForPart(self.getMovieSearchString(os.path.basename(part.file)))
                     for si in subsList:
-                        part.subtitles[si.lang][si.url] = Proxy.Media(si.sub, ext=si.subExt)
-                        valid_names.append(si.url)
-                        part.subtitles[si.lang].validate_keys(valid_names)
-                        Log("Validating keys...")
+                        part.subtitles[si.lang][si.url] = Proxy.Media(si.sub, ext=si.ext)
+
 
 class HdbitsSubtitlesAgentMovies(Agent.TV_Shows):
     name = 'HDBits.org TV Subtitles'
@@ -144,16 +165,13 @@ class HdbitsSubtitlesAgentMovies(Agent.TV_Shows):
 
     def update(self, metadata, media, lang):
         Log("TvUpdate. Lang %s" % lang)
-        Log("media: %s" % media)
         for season in media.seasons:
             for episode in media.seasons[season].episodes:
                 for item in media.seasons[season].episodes[episode].items:
                     for part in item.parts:
-                        subsList = getSubsForPart(part)
+                        searchWords = [media.title, getSnEString(season, episode), getReleaseGroup(part.file)]
+                        searchString = string.join(searchWords, ' ').strip()
+                        subsList = getSubsForPart(searchString)
                         Log("Found %d subs" % len(subsList))
-                        valid_names = list()
                         for si in subsList:
-                            part.subtitles[si.lang][si.url] = Proxy.Media(si.sub, ext=si.subExt)
-                            valid_names.append(si.url)
-                            Log("Validating keys...")
-                            part.subtitles[si.lang].validate_keys(valid_names)
+                            part.subtitles[si.lang][si.url] = Proxy.Media(si.sub, ext=si.ext)
